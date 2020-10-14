f8577b54dd38e98871c52fde15e45f9c954bab8f
sebastianaf <sebastian.afanador@correounivalle.edu.co>
2020-10-14 12:25:38 -0500

    Correcciones

--- src/colegio/Colegio.java
+++ src/colegio/Colegio.java
@@ -3,7 +3,6 @@ package colegio;
 import java.io.PrintStream;
 import java.util.Date;
 
-
 public class Colegio {
 
     private String nombre;// nombre del colegio
@@ -11,44 +10,45 @@ public class Colegio {
     private Date fechaDeFundacion; //fecha en la que se fundo el colegio
     private int numeroDeSalones;//numero de salones que hay en el colegio
     private Boolean nacional; //si es conocidad en colombia     
-    private int numeroDeProfesoresNuevo;//es la cantidad de profesores nuevos en el colegio 
-    private int numeroDeAlumnosEnCadaSalon;//es la cantidad de alumnos en cada salon
-    private int numeroDeAlumnosQueTransfieran;//es la cantidad de alumnos que pagan la matricula
+    //private int numeroDeProfesoresNuevo;//es la cantidad de profesores nuevos en el colegio 
+    //private int numeroDeAlumnosEnCadaSalon;//es la cantidad de alumnos en cada salon
+    //private int numeroDeAlumnosQueTransfieran;//es la cantidad de alumnos que pagan la matricula
 
     //este es el constructor de clases
-    public Colegio(String nombre, 
-            int numeroDeProfesores, 
-            Date fechaDeFundacion, 
-            int numeroDeSalones, 
-            Boolean nacional,
-            int numeroDeProfesoresNuevo,
-            int numeroDeAlumnosEnCadaSalon,
-            int numeroDeAlumnosQueTransfieran) {
+    public Colegio(String nombre,
+            int numeroDeProfesores,
+            Date fechaDeFundacion,
+            int numeroDeSalones,
+            Boolean nacional
+    //int numeroDeProfesoresNuevo,
+    //int numeroDeAlumnosEnCadaSalon,
+    //int numeroDeAlumnosQueTransfieran
+    ) {
         this.nombre = nombre;
         this.numeroDeProfesores = numeroDeProfesores;
         this.fechaDeFundacion = fechaDeFundacion;
         this.numeroDeSalones = numeroDeSalones;
         this.nacional = nacional;
-        this.numeroDeProfesoresNuevo = numeroDeProfesoresNuevo;
-        this.numeroDeAlumnosEnCadaSalon = numeroDeAlumnosEnCadaSalon;
-        this.numeroDeAlumnosQueTransfieran = numeroDeAlumnosQueTransfieran;
+        //this.numeroDeProfesoresNuevo = numeroDeProfesoresNuevo;
+        //this.numeroDeAlumnosEnCadaSalon = numeroDeAlumnosEnCadaSalon;
+        //this.numeroDeAlumnosQueTransfieran = numeroDeAlumnosQueTransfieran;
     }
 
-   
-   //aumenta la cantidad de salones en 20 y aumenta el numero de profesores en 15
+    //aumenta la cantidad de salones en 20 y aumenta el numero de profesores en 15
     public void hacerMasSalones() {
-        this.numeroDeSalones +=9;
-        this.numeroDeProfesores +=2;
+        this.numeroDeSalones += 9;
+        this.numeroDeProfesores += 2;
     }
-    
+
     /**
-     * Este metodo permite cambiar el nombre del colegio y sea conocido nacionalmente 
+     * Este metodo permite cambiar el nombre del colegio y sea conocido
+     * nacionalmente
      */
-    public void reconocerNacionalmente(String nuevoNombre) { 
+    public void reconocerNacionalmente(String nuevoNombre) {
         this.nombre = nuevoNombre;
         this.nacional = true;
     }
-    
+
     // a partir de esta linea se encuentran los metodos get y set
     public String getNombre() {
         return nombre;
@@ -89,58 +89,35 @@ public class Colegio {
     public void setNacional(Boolean nacional) {
         this.nacional = nacional;
     }
-    
-    public int getNumeroDeProfesoresNuevo (){
-        return numeroDeProfesores; 
+
+    public int getNumeroDeProfesoresNuevo() {
+        return numeroDeProfesores;
     }
-    
-    public void setNumeroDeProfesoresNuevo (int numeroDeProfesoresNuevo) {
-              this.numeroDeProfesoresNuevo = numeroDeProfesoresNuevo;
-    }          
-    
-    public int getNumeroDeAlumnosEnCadaSalon () {
-        return numeroDeAlumnosEnCadaSalon;
-    }
-    
-    public void setNumeroDeAlumnosEnCadaSalon (int numeroDeAlumnosEnCadaSalon) {
-              this.numeroDeAlumnosEnCadaSalon = numeroDeAlumnosEnCadaSalon;
-    }       
-    
-    public int getNumeroDeAlumnosQueTransfieran () {
-        return numeroDeAlumnosQueTransfieran;
-    }
-    
-    public void setNumeroDeAlumnosQueTransfieran (int numeroDeAlumnosQueTranfieran) {
-              this.numeroDeAlumnosQueTransfieran = numeroDeAlumnosQueTranfieran;
-    
+
     public static void main(String[] args) {
-        Colegio colegioUno = new Colegio("Colegio los morales",
-                    8,
-                    new Date(),
-                    10,
-                    true,
-                    3,
-                    30,
-                    10);
-        Colegio colegioDos = new Colegio("Colegio monserrat",
-                    12,
-                    new Date(),
-                    20,
-                    false,
-                    4,
-                    40,
-                    10);
-        
+        Colegio colegioUno = new Colegio(
+                "Colegio los morales",
+                8,
+                new Date(),
+                10,
+                true
+        );
+        Colegio colegioDos = new Colegio(
+                "Colegio monserrat",
+                12,
+                new Date(),
+                20,
+                false);
+
         System.out.println(colegioUno.getNombre());
         System.out.println(colegioUno.getNumeroDeSalones());
         System.out.println(colegioUno.getNumeroDeProfesores());
-        
+
         colegioUno.hacerMasSalones();
-       
+
         System.out.println(colegioUno.getNombre());
         System.out.println(colegioUno.getNumeroDeSalones());
-        System.out.println(colegioUno.getNumeroDeProfesores());  
+        System.out.println(colegioUno.getNumeroDeProfesores());
     }
 
-    
 }
--- src/colegio/ColegioMixto.java
+++ src/colegio/ColegioMixto.java
@@ -2,15 +2,22 @@ package Colegio;
 
 import java.util.Date;
 
-
 public class ColegioMixto extends Colegio {
-    private int numeroDeProfesoresNuevos; /** Es la cantidad de profesores nuevos 
-    que hay en el colegio mixto*/
-    private int numeroDeAlumnosEnCadaSalon; /** Es la cantidad de alumnos que 
-     * hay en cada salon*/
-    private int numeroDeAlumnosQuetrasfieren; /** es la cantidad de alumnos que
-     * pagan la matricula haciendo tranferencia al colegio*/
-    public ColegioMixto ( 
+
+    private int numeroDeProfesoresNuevos;
+    /**
+     * Es la cantidad de profesores nuevos que hay en el colegio mixto
+     */
+    private int numeroDeAlumnosEnCadaSalon;
+    /**
+     * Es la cantidad de alumnos que hay en cada salon
+     */
+    private int numeroDeAlumnosQuetrasfieren;
+
+    /**
+     * es la cantidad de alumnos que pagan la matricula haciendo tranferencia al colegio
+     */
+    public ColegioMixto(
             String nombre,
             int numeroDeProfesores,
             Date fechaDeFundacion,
@@ -18,44 +25,71 @@ public class ColegioMixto extends Colegio {
             Boolean nacional,
             int numeroDeProfesoresNuevos,
             int numeroDeAlumnosEnCadaSalon,
-            int numeroDeAlumnosQueTrasfieren ) {
-        super (
-        
-            nombre,
-            numeroDeProfesores,
-            fechaDeFundacion,
-            numeroDeSalones,
-            nacional);
+            int numeroDeAlumnosQueTrasfieren) {
+        super(nombre,
+                numeroDeProfesores,
+                fechaDeFundacion,
+                numeroDeSalones,
+                nacional);
         this.numeroDeProfesoresNuevos = numeroDeProfesoresNuevos;
         this.numeroDeAlumnosEnCadaSalon = numeroDeAlumnosEnCadaSalon;
         this.numeroDeAlumnosQuetrasfieren = numeroDeAlumnosQueTrasfieren;
     }
-    
+
     //con este metodo sabremos la cantidad de alumnos que le hacen transferencia al colegio
-       public void numeroDePago(){
-         if(this.numeroDeAlumnosEnCadaSalon >=30){
-              this.numeroDeAlumnosQuetrasfieren >=25;
-          }
-       }
-       
-       //aumenta la cantidad de profesores nuevos en 2 y el numero de alumnos en 10
-       public void nuevosAlumnos(){
-           this.numeroDeProfesoresNuevos += 2;
-           this.numeroDeAlumnosEnCadaSalon += 10;
-       }
-        
-        public static void main(String[] args) {
-                ColegioMixto Colegio = new ColegioMixto (
-                    "Colegio Mixto Los Morales",
-                    8,
-                    new Date(),
-                    10,
-                    false,
-                    3,
-                    25,
-                    15);
-        ColegioMixto.nuevosAlumnos();      
-        System.out.println(ColegioMixto.get());   
+    public void numeroDePago() {
+        if (this.numeroDeAlumnosEnCadaSalon >= 30) {
+            this.numeroDeAlumnosQuetrasfieren = 25;
         }
-}
+    }
+
+    //aumenta la cantidad de profesores nuevos en 2 y el numero de alumnos en 10
+    public void nuevosAlumnos() {
+        this.numeroDeProfesoresNuevos += 2;
+        this.numeroDeAlumnosEnCadaSalon += 10;
+    }
+
+    public int getNumeroDeProfesoresNuevos() {
+        return numeroDeProfesoresNuevos;
+    }
+
+    public void setNumeroDeProfesoresNuevos(int numeroDeProfesoresNuevos) {
+        this.numeroDeProfesoresNuevos = numeroDeProfesoresNuevos;
+    }
+
+    public int getNumeroDeAlumnosEnCadaSalon() {
+        return numeroDeAlumnosEnCadaSalon;
+    }
+
+    public void setNumeroDeAlumnosEnCadaSalon(int numeroDeAlumnosEnCadaSalon) {
+        this.numeroDeAlumnosEnCadaSalon = numeroDeAlumnosEnCadaSalon;
+    }
+
+    public int getNumeroDeAlumnosQuetrasfieren() {
+        return numeroDeAlumnosQuetrasfieren;
+    }
+
+    public void setNumeroDeAlumnosQuetrasfieren(int numeroDeAlumnosQuetrasfieren) {
+        this.numeroDeAlumnosQuetrasfieren = numeroDeAlumnosQuetrasfieren;
+    }
+
+    public static void main(String[] args) {
+        ColegioMixto losMorales = new ColegioMixto(
+                "Colegio Mixto Los Morales",
+                8,
+                new Date(),
+                10,
+                false,
+                3,
+                25,
+                15);
+
+        System.out.println("el número de profesores es: ");
+        System.out.println(losMorales.getNumeroDeProfesoresNuevos());
         
+        losMorales.nuevosAlumnos();
+
+        System.out.println("el número de profesores ahora es: ");
+        System.out.println(losMorales.getNumeroDeProfesoresNuevos());
+    }
+}
